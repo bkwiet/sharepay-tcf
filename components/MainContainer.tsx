9b94979e761c2f1676dd2b51c6aa1671f8985c2f
Philippe Adisson <philippe.adisson.pro@gmail.com>
2020-11-23 16:24:32 +0100

    ajout components et player

---
+++ components/MainContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainContainer: React.FC = ({}) => {
+  return <>hello</>;
+};
+
+export default MainContainer;
---
+++ pages/player.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import MainContainer from "../components/MainContainer";
+
+const Player: NextPage<Props> = ({ accessToken }) => {
+  return <MainContainer />;
+};
+export default Player;
