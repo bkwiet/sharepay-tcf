fdd4ad68fd4731e99c86e8e273483ebdc750e6d9
bkwiet <x.crecel@gmail.com>
2020-11-24 09:31:39 +0100

    Lab trying next auth

---
+++ src/components/layout.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Head from "next/head";
+import Header from "./header";
+import styles from "../../styles/Layout.module.css";
+
+type Props = {
+  setPage: React.Dispatch<React.SetStateAction<string>>;
+  isLoggedIn: boolean;
+  spotifyLoginUrl: string;
+};
+
+export const Layout: React.FC<Props> = ({ children, isLoggedIn }) => {
+  // const { data } = useSWR("/api/get-cookies");
+  // const accessToken = data;
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="bkwiet corp icon" href="favicon.png" type="image/x-icon" />
+        <script src="https://kit.fontawesome.com/95a069202e.js" crossOrigin="anonymous"></script>
+      </Head>
+      <Header />
+      <div className={""}>
+        <main className={""}>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
+import { SWRConfig } from "swr";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const { session } = pageProps;
+  return (
+    <Provider session={session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
