24502f980900619ee4384cfda6057ff473570be0
20000321 <47658224+20000321@users.noreply.github.com>
2020-11-25 15:52:32 +0100

    Ple day3 (#19)
    * change name collection without Uppercase
    
    * acces lecture findUserById findUserByEmail findProjectById + page presentation
    
    Co-authored-by: UnpolariZ <70898929+UnpolariZ@users.noreply.github.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,45 @@ export const Header: React.FC = () => {
   return (
     <>
       <header>
+
+        <nav>
+          <p>
+            {!session && (
+              <a
+                href="/api/auth/signin"
+                onClick={(e) => {
+                  e.preventDefault();
+                  signin();
+                }}
+              >
+                <button className="signInButton">Sign in</button>
+              </a>
+            )}
+            {session && (
+              <>
+                <Link href="/profile">
+                  <a>
+                    <span style={{ backgroundImage: `url(${session.user.image})` }} className="avatar" />
+                  </a>
+                </Link>
+                <span className="email">{session.user.email}</span>
+                <a
+                  href="/api/auth/signout"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signout();
+                  }}
+                >
+                  <button className="signOutButton">Sign out</button>
+                </a>
+              </>
+            )}
+          </p>
+        </nav>
+
         <NavBar></NavBar>
         <nav> </nav>
+
 
         <style jsx>{``}</style>
       </header>
