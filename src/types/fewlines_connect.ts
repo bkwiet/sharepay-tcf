bf4907f4cb4ec67119c0f9d8e7c4d37bce52c607
UnpolariZ <70898929+UnpolariZ@users.noreply.github.com>
2020-11-25 14:36:58 +0100

    ajout structure css (#18)

--- src/types/fewlines_connect.ts
+++ src/types/fewlines_connect.ts
@@ -12,11 +12,16 @@ export type Profile = {
   name: string;
   aud: string[];
   email: string;
-  email_verified: bool;
+  email_verified: boolean;
   exp: number;
   iat: number;
   iss: string;
   sub: string;
 };
 
-export type User = { name: string; email: string; image: string; accessToken?: string };
+export type User = {
+  name: string;
+  email: string;
+  image: string;
+  accessToken?: string;
+};
