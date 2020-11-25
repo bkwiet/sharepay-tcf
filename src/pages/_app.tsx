d6dac646d996ef3a6a07e4d7c280cd5198fbbfe6
Xavier Crecel <21124635+bkwiet@users.noreply.github.com>
2020-11-25 09:12:19 +0100

    Dev002 next auth fewlines connect google GitHub (#17)
    * Lab trying next auth
    
    * Nextauth - Github Google : ok  - Fewlines On the go
    
    * All Nextauth AccessToken in it - Fewlines good to go | Next -> Fix database registration / Check an register user
    
    * mergin dev001 to  main

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../public/styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const { session } = pageProps;
+  return (
+    <Provider options={{ site: process.env.SITE }} session={session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import styles from "../../public/styles/Home.module.css";
+import Layout from "../components/layout";
+
+export default function Home() {
+  return <Layout></Layout>;
+}
