8c4e5c46d4b7783761d53d47f81ea4146ea2209b
bkwiet <x.crecel@gmail.com>
2020-11-26 14:07:02 +0100

    Adjustement

--- src/pages/auth/registration.tsx
+++ src/pages/auth/registration.tsx
@@ -1,18 +1,30 @@
 import React from "react";
-import { providers, signIn } from "next-auth/client";
+import { getSession } from "next-auth/client";
 import Layout from "../../components/layout";
+import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { Button } from "react-bootstrap";
-import styles from "../../../public/styles/Signin.module.css";
+import styles from "../../../public/styles/Registration.module.css";
 
-export default function Registration({ providers }) {
-  let icon;
+const Registration: NextPage = ({ session }) => {
   return (
     <>
       <Head>
         <title>Tout Compte Fait - Registration</title>
       </Head>
-      <Layout></Layout>
+      <Layout>{session && <h1>Hello {session.user.email}</h1>}</Layout>
     </>
   );
-}
+};
+
+export default Registration;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
