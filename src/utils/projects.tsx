da60f8b53a7175273415cfe541a4cfbab912d6fc
20000321 <47658224+20000321@users.noreply.github.com>
2020-11-27 18:19:02 +0100

    Ple day5 am (#41)
    * ple_day5_am
    
    * PLE ajout creation projects + affectation project to user creator
    
    Co-authored-by: Xavier Crecel <21124635+bkwiet@users.noreply.github.com>

--- src/utils/projects.tsx
+++ src/utils/projects.tsx
@@ -1,10 +1,25 @@
 
 import { getDatabase } from "./database"
 
-export async function findProjectById(idkey:string): Promise<void> {
+export async function findProjectById(idkey:Number): Promise<void> {
   console.log("appel ouverture getDatabase" );
   console.log("valeur de idkey",idkey);
   const mongodb = await getDatabase();
   const project = await mongodb.db().collection("projects").findOne({idkey: idkey});
   return project;
 };
+
+export async function newProjectIdKey(): Promise<number> {
+  const mongodb = await getDatabase();
+  const maxIdKey = await mongodb.db().collection("projects").find().sort({idkey:-1}).limit(1).toArray();
+
+  // return la nouvelle valeur user_idkey
+  let IdKey =0;
+  if (maxIdKey) {
+    IdKey = maxIdKey[0].idkey + 1;
+  } else {
+    IdKey = 1;
+  }
+    
+  return IdKey;
+}
