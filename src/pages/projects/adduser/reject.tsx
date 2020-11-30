f06396027749b44fafc0f52879992dde4ed00f40
Philippe Adisson <philippe.adisson.pro@gmail.com>
2020-11-30 15:19:39 +0100

    rajout du css

--- src/pages/projects/adduser/reject.tsx
+++ src/pages/projects/adduser/reject.tsx
@@ -1,23 +1,39 @@
 import { GetServerSideProps } from "next";
 import React from "react";
 import Layout from "../../../components/layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+import styles from "../../../../public/styles/Home.module.css";
+import Link from "next/link";
 
-const affAddUserReject: React.FC<{ }> = ({ }) => {
-
+const affAddUserReject: React.FC<{}> = ({}) => {
   return (
     <Layout>
       <div className="container">
         <div id="projetContainer" className="card mt-5">
           <div className="row">
-            <div className="col text-warning">
-              Ajout d'un USER à un projet
+            <span className={styles.spinout}>
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+            </span>
+            <div className="col text-warning" id="WarningText">
+              Erreur lors de l'ajout d'un utilisateur au projet
             </div>
+            <span className={styles.spinout}>
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+            </span>
           </div>
 
-          <div className="row">
+          <div className="row" id="warningTextbis">
             <div className="col">
-              <p>Soit l'email est identique à celui de l'administrateur</p>
-              <p>Soit l'email du nouveau user n'est pas trouvé dans le db</p>
+              <p>Soit l'email est identique à celui de l'administrateur.</p>
+              <p>
+                Soit l'email du nouvel utilisateur n'existe pas dans notre base
+                de donnée.
+              </p>
+              <p>
+                Pour reessayer,
+                <Link href={"/projects/adduser"}> click ICI</Link>
+              </p>
             </div>
           </div>
         </div>
@@ -28,10 +44,7 @@ const affAddUserReject: React.FC<{ }> = ({ }) => {
 export default affAddUserReject;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  
-    return {
-      props: {
-        
-      },
-    };
+  return {
+    props: {},
+  };
 };
