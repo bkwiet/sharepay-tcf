a72f09e54ebc77accdd74595b6f62496647d8283
Philippe Lefevre <philippe.lefevre@adeo.com>
2020-12-02 17:43:26 +0100

    correction mauvais merge project.tsx

--- src/pages/projects/show/[project].tsx
+++ src/pages/projects/show/[project].tsx
@@ -3,66 +3,95 @@ import React from "react";
 import { findProjectById } from "../../../utils/projects";
 import { Projects } from "../../../types/projects";
 import Layout from "../../../components/layout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDonate } from "@fortawesome/free-solid-svg-icons";
 
 
 const affOneProject: React.FC<{ project: Projects }> = ({ project }) => {
   console.log("arrivee typeof datas", typeof project);
   console.log(project);
 
+  // calcul du solde a payer sur le projet
+  let allpayment:number = 0;
+  project.payments.map((paiement)=>{
+    allpayment = allpayment + Number(paiement.amount);
+  })
+  const solde = project.amount - allpayment;
+  // fin de calcul du solde
+
   return (
     <Layout>
       <div className="container">
-        <div className="mt-5">
+
+        <div className="mt-1">
           <div className="row">
-            <div className="col">
-              <h1 className="col text-center">{project.name}</h1>
-              <p className="col p-3 text-justify">{project.summary}</p>
-            </div>
+            <h1 className="col text-center">{project.name}</h1>
           </div>
 
+          <div className="row text-dark">
+            <h6>Summary</h6>
+          </div>
           <div className="row">
-            <h6 className="col p-3 text-justify">Participants</h6>
+              <p className="col">{project.summary}</p>   
+          </div>
+
+          <div className="row text-dark">
+            <h6>Budget</h6>
+          </div>
+          <div className="row">
+              <p className="col">{"Inital : " +project.amount + "€ - " + "All Payment : "+ allpayment+ " € - SOLD TO PAY : " + solde + " €"}</p>   
+          </div>
+
+
+          <div className="row text-dark">
+            <h6>User(s)</h6>
+          </div> 
+          <div>
             {project.users.map((user) => {
-              return (
-                <div className="col">
-                  <p>{user.firstname + " " +user.lastname}</p>
-                </div>
-              );
-            })}
+                return (
+                  <div className="row">
+                    <p className="col">{user.firstname + " " +user.lastname}</p>
+                  </div>
+                );
+              })}
           </div>
+          
 
-          <div className="row">
-            <h6 className="col p-3 text-justify">Payment(s)</h6>
+          <div className="row text-dark">
+            <h6>Payment(s)</h6>
           </div>
-
           <div>
             {project.payments.map((payment) => {
               return (
-                <div className="row">
-                  <p className="col ml-6">{payment.date_payment}</p>
-                  <p className="col ml-6">{payment.amount + "€"}</p>
-                  <p>{payment.user_idkey}</p>
-                  <p>{payment.summary}</p>
-                  
-                  
+                
+                <div>
+                  <div className="row">
+                    <p className="col-3">{payment.date_payment}</p>
+                    <p className="col-3">{payment.amount} €</p>
+                    {/* <p className="col-3">{"by "+ payment.user_idkey}</p> */}
+                  </div>
+                  <div className="row">
+                    <p className="col">{payment.summary}</p>
+                  </div>
                 </div>
+                
               );
             })}
           </div>
 
-
-        <div className="payment">
-          <a id="payment" href="#">
-            <span>
-              <div>
-                <span>
-                  <FontAwesomeIcon icon={faDonate} id="iconDonate" />
-                </span>
-              </div>
-              Add a Payment!
-            </span>
-          </a>
         </div>
+          <div className="payment">
+            <a id="payment" href="#">
+              <span>
+                <div>
+                  <span>
+                    <FontAwesomeIcon icon={faDonate} id="iconDonate" />
+                  </span>
+                </div>
+                Add a Payment!
+              </span>
+            </a>
+          </div>
       </div>
     </Layout>
   );
